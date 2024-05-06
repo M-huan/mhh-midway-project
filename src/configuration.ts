ee76874175ed348978c6ee72ebb4c0ff89626363
mahuanhuan <mahuanhuan@enn.cn>
2024-05-06 15:34:13 +0800

    feat: 初始化

---
+++ src/configuration.ts
@@ -0,0 +1,40 @@
+import { Configuration, App } from '@midwayjs/core';
+import * as koa from '@midwayjs/koa';
+import * as validate from '@midwayjs/validate';
+import * as info from '@midwayjs/info';
+import { join } from 'path';
+// import { DefaultErrorFilter } from './filter/default.filter';
+// import { NotFoundFilter } from './filter/notfound.filter';
+import { ReportMiddleware } from './middleware/report.middleware';
+// import {
+//   buc,
+//   getTokenHandler,
+//   BUC_ROUTER_PATH,
+//   logoutHandler,
+//   getIcomeTokenHandler,
+// } from '@enncloud/koa-buc-acl';
+
+@Configuration({
+  imports: [
+    koa,
+    validate,
+    {
+      component: info,
+      enabledEnvironment: ['local'],
+    },
+  ],
+  importConfigs: [join(__dirname, './config')],
+})
+export class MainConfiguration {
+  @App('koa')
+  app: koa.Application;
+
+  async onReady() {
+    // add middleware
+    this.app.useMiddleware([ReportMiddleware]);
+    // add filter
+    // this.app.useFilter([NotFoundFilter, DefaultErrorFilter]);
+    //在应用生命周期中onReady添加
+    // this.app.useMiddleware([buc(bucConfig)]);
+  }
+}
